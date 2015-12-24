036c556eb394728faf4fb4d58ed79b5e131c9017
Sahil Dua <sahildua2305@gmail.com>
2015-12-25 04:26:02 +0530

    Modified Models

--- polls/models.py
+++ polls/models.py
@@ -3,17 +3,29 @@
 # @Author: sahildua2305
 # @Date:   2015-12-25 03:17:44
 # @Last Modified by:   sahildua2305
-# @Last Modified time: 2015-12-25 03:41:35
+# @Last Modified time: 2015-12-25 04:16:55
 
 from __future__ import unicode_literals
 
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 
+	def __str__(self):
+		return self.question_text
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.choice_text
