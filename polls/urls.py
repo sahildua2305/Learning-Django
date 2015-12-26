92bb5d5c590bda4224281feeedf8915b58d1122c
Sahil Dua <sahildua2305@gmail.com>
2015-12-27 00:04:48 +0530

    Done with basic views

--- polls/urls.py
+++ polls/urls.py
@@ -2,13 +2,21 @@
 # -*- coding: utf-8 -*-
 # @Author: sahildua2305
 # @Date:   2015-12-25 03:17:44
-# @Last Modified by:   sahildua2305
-# @Last Modified time: 2015-12-25 03:31:27
+# @Last Modified by:   Sahil Dua
+# @Last Modified time: 2015-12-26 23:59:27
 
 from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
+	# ex: /polls/
 	url(r'^$', views.index, name='index'),
+	# ex: /polls/5/
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	# ex: /polls/5/results/
+	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+	# ex: /polls/5/vote/
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,10 +2,31 @@
 # -*- coding: utf-8 -*-
 # @Author: sahildua2305
 # @Date:   2015-12-25 03:17:44
-# @Last Modified by:   sahildua2305
-# @Last Modified time: 2015-12-25 03:31:31
+# @Last Modified by:   Sahil Dua
+# @Last Modified time: 2015-12-26 23:47:57
 
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 def index(request):
-	return HttpResponse("Hello, at index!")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+	return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	context = {
+		'question': question
+	}
+	return render(request, 'polls/detail.html', context)
+
+def results(request, question_id):
+	response = "Results of question %s."
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s." % question_id)
