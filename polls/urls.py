449ba09710213a635f8972fda18054084ff7395d
Sahil Dua <sahildua2305@gmail.com>
2015-12-28 01:36:24 +0530

    Added forms and generic views

--- polls/urls.py
+++ polls/urls.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # @Author: sahildua2305
 # @Date:   2015-12-25 03:17:44
-# @Last Modified by:   Sahil Dua
-# @Last Modified time: 2015-12-26 23:59:27
+# @Last Modified by:   sahildua2305
+# @Last Modified time: 2015-12-28 01:09:43
 
 from django.conf.urls import url
 
@@ -12,11 +12,11 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
 	# ex: /polls/
-	url(r'^$', views.index, name='index'),
+	url(r'^$', views.IndexView.as_view(), name='index'),
 	# ex: /polls/5/
-	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 	# ex: /polls/5/results/
-	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+	url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 	# ex: /polls/5/vote/
 	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
